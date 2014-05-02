13fdf801794487066558e81b9439b8fa1737f07e
mji <marek.jagielski@gmail.com>
2014-05-03 01:44:50 +0200

    Java Task : MainDataDecimalDims

---
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/data/MainDataDecimalDims.java
@@ -0,0 +1,35 @@
+package tasks.data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Decimal;
+
+@JavaTaskInfo
+public class MainDataDecimalDims extends JavaTask {
+
+	@InputPortInfo(name = "In", dataType = Decimal.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Decimal.class)
+	public OutputPort out;
+
+	@Override
+	public void execute() {
+		Decimal inDecimal = in.getData(Decimal.class);
+		List<BigDecimal> outdecimal = new ArrayList<>(inDecimal.getNumberOfElements());
+		int i = 0;
+		for(BigDecimal v : inDecimal.getValues()) {
+			outdecimal.add(v.movePointRight(i));
+			i++;
+		}
+		out.putData(new Decimal(inDecimal.getDimensions(), outdecimal));
+	}
+
+}
