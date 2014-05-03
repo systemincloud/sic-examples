f56d3a8f02a6e708f6490a82b7c4fe17f0435125
mji <marek.jagielski@gmail.com>
2014-05-03 13:00:40 +0200

    Java Task : MainDataTextDims

---
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/data/MainDataTextDims.java
@@ -0,0 +1,30 @@
+package tasks.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Text;
+
+@JavaTaskInfo
+public class MainDataTextDims extends JavaTask {
+
+	@InputPortInfo(name = "In", dataType = Text.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Text.class)
+	public OutputPort out;
+
+	@Override
+	public void execute() {
+		Text inText = in.getData(Text.class);
+		List<String> outtext = new ArrayList<>(inText.getNumberOfElements());
+		for(String v : inText.getValues())
+			outtext.add(v.toUpperCase());
+		out.putData(new Text(inText.getDimensions(), outtext));
+	}
+}
