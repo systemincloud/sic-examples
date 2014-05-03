5e4da024b12b7a556cc6ccb27b6bba22463da297
mji <marek.jagielski@gmail.com>
2014-05-03 12:42:06 +0200

    Java Task : MainDataInt64Dims

---
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/data/MainDataInt64Dims.java
@@ -0,0 +1,30 @@
+package tasks.data;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Int64;
+
+@JavaTaskInfo
+public class MainDataInt64Dims extends JavaTask {
+
+	@InputPortInfo(name = "In", dataType = Int64.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Int64.class)
+	public OutputPort out;
+
+	@Override
+	public void execute() {
+		Int64 inInt64 = in.getData(Int64.class);
+		long[] inint64 = inInt64.getValues();
+		long[] outint = new long[inInt64.getNumberOfElements()];
+		for(int i = 0; i < inInt64.getNumberOfElements(); i++)
+			outint[i] = inint64[i]*2;
+			
+		out.putData(new Int64(inInt64.getDimensions(), outint));
+	}
+
+}
