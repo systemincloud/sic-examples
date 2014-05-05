c40bb915de71c0af6a30f6866b0caf0d8d6d0c65
mji <marek.jagielski@gmail.com>
2014-05-05 23:33:43 +0200

    Java Task : MainAtomic2SIn2AIn1Out

---
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/ports/MainAtomic2SIn2AIn1Out.java
@@ -0,0 +1,38 @@
+package tasks.ports;
+
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Text;
+@JavaTaskInfo
+public class MainAtomic2SIn2AIn1Out extends JavaTask {
+
+	@InputPortInfo(name = "In1", dataType = Text.class)
+	public InputPort in1;
+	@InputPortInfo(name = "In2", dataType = Text.class)
+	public InputPort in2;
+	@InputPortInfo(name = "In3", dataType = Text.class, asynchronus = true)
+	public InputPort in3;
+	@InputPortInfo(name = "In4", dataType = Text.class, asynchronus = true)
+	public InputPort in4;
+	@OutputPortInfo(name = "Out", dataType = Text.class)
+	public OutputPort out;
+
+	@Override
+	public void executeAsync(InputPort asynchIn) {
+		Text asynchInText = asynchIn.getData(Text.class);
+		out.putData(new Text("From port " + asynchIn.getName() + ": " + asynchInText.getValue()));
+	}
+
+	@Override
+	public void execute() {
+		Text in1Text = in1.getData(Text.class);
+		Text in2Text = in2.getData(Text.class);
+		out.putData(new Text(in1Text.getValue() + " " + in2Text.getValue()));
+	}
+
+}
