a69f28173453124e60342f564544160afc357eb0
mji <marek.jagielski@gmail.com>
2014-05-05 17:36:09 +0200

    JavaTask : MainGenerator

---
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/generator/MainGenerator.java
@@ -0,0 +1,20 @@
+package tasks.generator;
+
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Text;
+
+@JavaTaskInfo(generator = true)
+public class MainGenerator extends JavaTask {
+
+	@OutputPortInfo(name = "Out", dataType = Text.class)
+	public OutputPort out;
+
+	@Override
+	public void generate() {
+		out.putData(new Text("Hello World"));
+		sleep(3000);
+	}
+}
