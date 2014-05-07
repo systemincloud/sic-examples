f3ba5589cd36bb4541712eb28d35532b2473bb97
mji <marek.jagielski@gmail.com>
2014-05-07 14:08:30 +0200

    Java Task : MainConstant2

---
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/constant/MainConstant2Add.java
@@ -0,0 +1,26 @@
+package tasks.constant;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Int32;
+
+@JavaTaskInfo
+public class MainConstant2Add extends JavaTask {
+
+	@InputPortInfo(name = "In1", dataType = Int32.class)
+	public InputPort in1;
+	@InputPortInfo(name = "In2", dataType = Int32.class)
+	public InputPort in2;
+	@OutputPortInfo(name = "Out", dataType = Int32.class)
+	public OutputPort out;
+
+	@Override
+	public void execute() {
+		out.putData(new Int32(in1.getData(Int32.class).getValue() + in2.getData(Int32.class).getValue()));
+	}
+
+}
