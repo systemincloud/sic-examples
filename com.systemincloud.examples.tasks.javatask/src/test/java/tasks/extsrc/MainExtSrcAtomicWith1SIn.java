60ce98c60e20fc18543aa02a3b42a11123ca340f
mji <marek.jagielski@gmail.com>
2014-05-08 12:20:40 +0200

    Java Task : MainExtSrcAtomicWith1SIn

---
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/extsrc/MainExtSrcAtomicWith1SIn.java
@@ -0,0 +1,43 @@
+package tasks.extsrc;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Int32;
+import com.systemincloud.modeler.tasks.javatask.api.data.Text;
+
+@JavaTaskInfo(externalSource = true)
+public class MainExtSrcAtomicWith1SIn extends JavaTask {
+
+	{
+	    new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					try { Thread.sleep(2000);
+					} catch (InterruptedException e) { }
+					externalData();
+				}
+			}
+		}).start();
+	}
+	
+	@InputPortInfo(name = "In", dataType = Int32.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Text.class)
+	public OutputPort out;
+
+	@Override
+	public void executeExtSrc() {
+		out.putData(new Text("External data"));
+	}
+
+	@Override
+	public void execute() {
+		out.putData(new Text("From In: " + in.getData(Int32.class).getValue()));
+	}
+
+}
