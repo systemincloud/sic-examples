2d3a45f4d42de870d5e1328a5b5c7b3a5ed72776
mji <marek.jagielski@gmail.com>
2014-05-10 21:56:10 +0200

    Validation : update diagram

--- com.systemincloud.examples.validation/src/test/java/tasks/datacast/DataInt64Casted.java
+++ com.systemincloud.examples.validation/src/test/java/tasks/datacast/DataInt64Casted.java
@@ -6,21 +6,20 @@ import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
-import com.systemincloud.modeler.tasks.javatask.api.data.Int64;
+import com.systemincloud.modeler.tasks.javatask.api.data.Control;
 import com.systemincloud.modeler.tasks.javatask.api.data.Text;
 
 @JavaTaskInfo
 public class DataInt64Casted extends JavaTask {
 
-	@InputPortInfo(name = "In", dataType = Int64.class)
+	@InputPortInfo(name = "In", dataType = Control.class)
 	public InputPort in;
 	@OutputPortInfo(name = "Out", dataType = Text.class)
 	public OutputPort out;
 
 	@Override
 	public void execute() {
-		Int64 inInt64 = in.getData(Int64.class);
-		out.putData(new Text("Casted " + inInt64.getValue()));
+		out.putData(new Text("Received"));
 	}
 
 }
