45dbcd2045b2df1be10ebcefc2e3ce4b483ec126
mji <marek.jagielski@gmail.com>
2014-05-11 00:55:34 +0200

    Embedded Task : Atomic1SIn

---
+++ com.systemincloud.examples.tasks.embeddedtask/src/test/java/tasks/ports/Atomic1SIn.java
@@ -0,0 +1,24 @@
+package tasks.ports;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Bool;
+
+@JavaTaskInfo
+public class Atomic1SIn extends JavaTask {
+
+	@InputPortInfo(name = "In", dataType = Bool.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Bool.class)
+	public OutputPort out;
+
+	@Override
+	public void execute() {
+		out.putData(new Bool(!in.getData(Bool.class).getValue()));
+	}
+
+}
