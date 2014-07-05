8ff14933c2474255a70e26762122e245ffc39398
mji <marek.jagielski@gmail.com>
2014-07-05 15:45:37 +0200

    Console : raw

---
+++ com.systemincloud.examples.tasks.console/src/test/java/tasks/data/DataRaw.java
@@ -0,0 +1,21 @@
+package tasks.data;
+
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Raw;
+
+@JavaTaskInfo(generator = true)
+public class DataRaw extends JavaTask {
+
+	@OutputPortInfo(name = "Out", dataType = Raw.class)
+	public OutputPort out;
+	
+	@Override
+	public void generate() {
+		byte[] bytes = "String".getBytes();
+		out.putData(new Raw(bytes));
+	}
+
+}
