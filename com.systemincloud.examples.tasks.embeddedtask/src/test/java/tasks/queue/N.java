0e6cfab551bf6d5da463cf874801322ff0b18127
mji <marek.jagielski@gmail.com>
2014-07-12 19:41:33 +0200

    EmbededTask : queue

---
+++ com.systemincloud.examples.tasks.embeddedtask/src/test/java/tasks/queue/N.java
@@ -0,0 +1,31 @@
+package tasks.queue;
+
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.SicParameters;
+import com.systemincloud.modeler.tasks.javatask.api.data.Int32;
+
+@JavaTaskInfo(generator = true)
+@SicParameters(names={N.INTERVAL})
+public class N extends JavaTask {
+
+	@OutputPortInfo(name = "Out", dataType = Int32.class)
+	public OutputPort out;
+
+	protected static final String INTERVAL = "INTERVAL";
+	
+	private int i = 0;
+	
+	@Override
+	public void runnerStart() {
+		sleep(Integer.parseInt(getParameter(INTERVAL)));
+	};
+	
+	@Override
+	public void generate() {
+		out.putData(new Int32(i++));
+	}
+
+}
