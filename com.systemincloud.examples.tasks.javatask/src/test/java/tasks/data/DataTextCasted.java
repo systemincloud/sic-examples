f28a7faefa8996a592d4327588f497df14c20d63
mji <marek.jagielski@gmail.com>
2014-07-14 13:18:18 +0200

    Add parents task to ports

--- com.systemincloud.examples.tasks.javatask/src/test/java/tasks/data/DataTextCasted.java
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/data/DataTextCasted.java
@@ -6,12 +6,13 @@ import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
 import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Control;
 import com.systemincloud.modeler.tasks.javatask.api.data.Text;
 
 @JavaTaskInfo
 public class DataTextCasted extends JavaTask {
 
-	@InputPortInfo(name = "In", dataType = Text.class)
+	@InputPortInfo(name = "In", dataType = {Text.class, Control.class})
 	public InputPort in;
 	@OutputPortInfo(name = "Out", dataType = Text.class)
 	public OutputPort out;
