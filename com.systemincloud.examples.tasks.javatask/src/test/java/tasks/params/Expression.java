4c2a541ffb5867d179149e99e4d1a09030832cbe
mji <marek.jagielski@gmail.com>
2014-12-14 20:26:26 +0100

    expression

---
+++ com.systemincloud.examples.tasks.javatask/src/test/java/tasks/params/Expression.java
@@ -0,0 +1,37 @@
+package tasks.params;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.SicParameters;
+import com.systemincloud.modeler.tasks.javatask.api.data.Int32;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.SicParameter;
+
+@JavaTaskInfo
+@SicParameters({
+	@SicParameter(name=Expression.N)
+}) public class Expression extends JavaTask {
+
+	protected static final String N = "N";
+
+	@InputPortInfo(name = "In", dataType = Int32.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Int32.class)
+	public OutputPort out;
+
+	private int n;
+
+	@Override
+	public void runnerStart() {
+		this.n = (Integer) runExpression(getParameter(N));
+	};
+
+	@Override
+	public void execute() {
+		out.putData(new Int32(in.getData(Int32.class).getValue()*n));
+	}
+
+}
