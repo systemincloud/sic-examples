dbbd40035f0e8a19c97204e11812fdebdd757cd7
mji <marek.jagielski@gmail.com>
2015-01-31 01:06:31 +0100

    synchronization : multiple ticks

---
+++ com.systemincloud.examples.synchronization/src/test/java/tasks/ticks/MutlipleTicks.java
@@ -0,0 +1,26 @@
+package tasks.ticks;
+
+import com.systemincloud.modeler.tasks.javatask.api.InputPort;
+import com.systemincloud.modeler.tasks.javatask.api.JavaTask;
+import com.systemincloud.modeler.tasks.javatask.api.OutputPort;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.InputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.JavaTaskInfo;
+import com.systemincloud.modeler.tasks.javatask.api.annotations.OutputPortInfo;
+import com.systemincloud.modeler.tasks.javatask.api.data.Bool;
+
+@JavaTaskInfo
+public class MutlipleTicks extends JavaTask {
+
+	@InputPortInfo(name = "In", dataType = Bool.class)
+	public InputPort in;
+	@OutputPortInfo(name = "Out", dataType = Bool.class)
+	public OutputPort out;
+
+	@Override
+	public void execute() {
+		boolean inValue = in.getData(Bool.class).getValue();
+		out.putData(new Bool(inValue));
+		out.putData(new Bool(!inValue));
+	}
+
+}
