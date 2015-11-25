3546dea81239e3a6e74c21be00cfbfc61a3b5ff3
mji <marek.jagielski@gmail.com>
2015-11-25 21:13:10 +0100

    python : Constant

--- com.systemincloud.examples.tasks.pythontask/src/test/py/tasks/constant/Constant1.py
+++ com.systemincloud.examples.tasks.pythontask/src/test/py/tasks/constant/Constant1.py
@@ -1,18 +1,13 @@
 from sicpythontask.PythonTaskInfo import PythonTaskInfo
 from sicpythontask.PythonTask import PythonTask
 from sicpythontask.OutputPort import OutputPort
-from sicpythontask.data.Text import Text
+from sicpythontask.data.Bool import Bool
 
 @PythonTaskInfo(constant=True)
 class Constant1(PythonTask):
 
     def __init_ports__(self):
-        self.out = OutputPort(name="Out", data_type=Text)
-
-    def __init__(self):
-        print("yyyyy")
+        self.out = OutputPort(name="Out", data_type=Bool)
 
     def execute(self, grp):
-        print("xxxxx")
-        self.logger.error("vvvv")
-        self.out.put_data(Text("Hello World"))
+        self.out.put_data(Bool([False, True], [1, 2]))
