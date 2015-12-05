96707779db83f9ddd701c713fbb79b6f7e1b9d79
mji <marek.jagielski@gmail.com>
2015-12-05 23:56:17 +0100

    py : DataComplex64Dims

---
+++ com.systemincloud.examples.tasks.pythontask/src/test/py/tasks/data/DataComplex64Dims.py
@@ -0,0 +1,20 @@
+from sicpythontask.PythonTaskInfo import PythonTaskInfo
+from sicpythontask.PythonTask import PythonTask
+from sicpythontask.InputPort import InputPort
+from sicpythontask.OutputPort import OutputPort
+from sicpythontask.data.Complex64 import Complex64
+from _operator import add
+
+@PythonTaskInfo
+class DataComplex64Dims(PythonTask):
+
+    def __init_ports__(self):
+        self.in1 = InputPort(name="in1", data_type=Complex64)
+        self.in2 = InputPort(name="in2", data_type=Complex64)
+        self.out = OutputPort(name="out", data_type=Complex64)
+
+    def execute(self, grp):
+        in1Data = self.in1.get_data(Complex64);
+        in2Data = self.in2.get_data(Complex64);
+        ret = list(map(add, in1Data.values, in2Data.values))
+        self.out.put_data(Complex64(ret, in1Data.dims));
