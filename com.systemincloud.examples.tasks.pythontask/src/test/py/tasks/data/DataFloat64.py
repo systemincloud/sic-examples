7b7c582bb67d2f2a1c153eb9cc6177cd12994d2e
mji <marek.jagielski@gmail.com>
2015-12-07 22:36:02 +0100

    py : DataFloat64 & py : DataFloat64Dims

---
+++ com.systemincloud.examples.tasks.pythontask/src/test/py/tasks/data/DataFloat64.py
@@ -0,0 +1,18 @@
+from sicpythontask.PythonTaskInfo import PythonTaskInfo
+from sicpythontask.PythonTask import PythonTask
+from sicpythontask.InputPort import InputPort
+from sicpythontask.OutputPort import OutputPort
+from sicpythontask.data.Float64 import Float64
+
+@PythonTaskInfo
+class DataFloat64(PythonTask):
+
+    def __init_ports__(self):
+        self.in1 = InputPort(name="in1", data_type=Float64)
+        self.in2 = InputPort(name="in2", data_type=Float64)
+        self.out = OutputPort(name="out", data_type=Float64)
+
+    def execute(self, grp):
+        in1Data = self.in1.get_data(Float64)
+        in2Data = self.in2.get_data(Float64)
+        self.out.put_data(Float64(in1Data.values[0] + in2Data.values[0]))
