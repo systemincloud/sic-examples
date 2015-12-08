dbff6db6909993f418456536816dda6f0cdd83a9
mji <marek.jagielski@gmail.com>
2015-12-08 23:50:03 +0100

    py : DataTextDims

---
+++ com.systemincloud.examples.tasks.pythontask/src/test/py/tasks/data/DataTextDims.py
@@ -0,0 +1,18 @@
+from sicpythontask.PythonTaskInfo import PythonTaskInfo
+from sicpythontask.PythonTask import PythonTask
+from sicpythontask.InputPort import InputPort
+from sicpythontask.OutputPort import OutputPort
+from sicpythontask.data.Text import Text
+
+@PythonTaskInfo
+class DataTextDims(PythonTask):
+
+    def __init_ports__(self):
+        self.in_ = InputPort(name="in", data_type=Text)
+        self.out = OutputPort(name="out", data_type=Text)
+
+    def execute(self, grp):
+        inData = self.in_.get_data(Text)
+        inRaw = inData.values
+        print(inRaw)
+        self.out.put_data(Text(inRaw, inData.dims))
