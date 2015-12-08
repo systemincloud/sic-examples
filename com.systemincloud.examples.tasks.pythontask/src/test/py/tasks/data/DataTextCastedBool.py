1d69b58dbc2d3e332f0248959a1d73a1065b3bfd
mji <marek.jagielski@gmail.com>
2015-12-08 23:55:11 +0100

    py : DataTextCastedBool

---
+++ com.systemincloud.examples.tasks.pythontask/src/test/py/tasks/data/DataTextCastedBool.py
@@ -0,0 +1,18 @@
+from sicpythontask.PythonTaskInfo import PythonTaskInfo
+from sicpythontask.PythonTask import PythonTask
+from sicpythontask.InputPort import InputPort
+from sicpythontask.OutputPort import OutputPort
+from sicpythontask.data.Text import Text
+from sicpythontask.data.Bool import Bool
+
+@PythonTaskInfo
+class DataTextCastedBool(PythonTask):
+
+    def __init_ports__(self):
+        self.in_ = InputPort(name="in", data_type=Bool)
+        self.out = OutputPort(name="out", data_type=Text)
+
+    def execute(self, grp):
+        inData = self.in_.get_data(Text)
+        self.out.put_data(Text(inData.values[0].upper()))
+
