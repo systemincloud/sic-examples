c9f1cff65627474798bcb8c85a70d0ebb6f04b50
mji <marek.jagielski@gmail.com>
2016-05-16 00:02:59 +0200

    r : break

--- com.systemincloud.examples.tasks.rtask/src/test/r/break/Break.R
+++ com.systemincloud.examples.tasks.rtask/src/test/r/break/Break.R
@@ -5,7 +5,7 @@ library(sicrtask)
 Break <- R6Class("Break",
   inherit = RTask,
   public = list(
-    rtaskinfo <- RTaskInfo$new(),
+    rtaskinfo = RTaskInfo$new(),
     in1 = NA,
     initialize_ports = function() {
       self$in1 <- InputPort$new(name = "in1", data_type = Int32)
@@ -14,9 +14,9 @@ Break <- R6Class("Break",
       self$initialize_ports()
     },
     execute = function(grp) {
-	  i = self$in1$get_data(Int32)$values[0]
-	  if( i == 3 ) self$exit_runner()
-	},
+	  #i = self$in1$get_data(Int32)$values[0]
+	  #if( i == 3 ) self$exit_runner()
+	}
   ),
   private = list(
   )
